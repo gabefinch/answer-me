bd49dc7eccda77016fed163b85c63848585aee53
Gabe Finch and Mike Goren <mgoren@mortalwombat.net>
2015-03-24 16:14:15 -0700

    change vote add method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,14 @@
 class VotesController < ApplicationController
 
   def create
-    user = current_user
-    response = Response.find(vote_params[:response_id])
-    Vote.create(user_id: user.id, response_id: response.id)
-    redirect_to question_path(response.question)
+    vote = Vote.new(vote_params)
+    vote.user = current_user
+    vote.save
+    redirect_to question_path(vote.response.question)
+    # alternate method:
+    # response = Response.find(vote_params[:response_id])
+    # Vote.create(user_id: user.id, response_id: response.id)
+    # redirect_to question_path(response.question)
   end
 private
   def vote_params
