d2e306cd6c5fadf7a397c3cd1ce78eeab5162431
Gabe Finch and Mike Goren <mgoren@mortalwombat.net>
2015-03-24 16:26:48 -0700

    uniqueify votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :response
+  validates :user_id, uniqueness: {scope: :response_id}
+
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Vote do
   it { should belong_to :user }
   it { should belong_to :response }
+  it { should validate_uniqueness_of(:user_id).scoped_to(:response_id) }
 end
